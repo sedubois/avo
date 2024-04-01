d5f32dbcca78ef52c8f1317517b4cc3e315a81b2
Sébastien Dubois <sedubois@users.noreply.github.com>
2024-04-01 17:14:28 +0200

    test: failing spec for #2636

--- spec/features/avo/field_sorting_spec.rb
+++ spec/features/avo/field_sorting_spec.rb
@@ -3,6 +3,9 @@ require "rails_helper"
 RSpec.feature "Field Sorting", type: :feature do
   let!(:user) { create :user }
   let!(:project) { create :project }
+  let!(:amy) { create :person, name: "amy" }
+  let!(:bob) { create :person, name: "Bob" }
+  let!(:cathy) { create :person, name: "Cathy" }
 
   describe "sortable field option" do
     it "provides the ability to sort by the ID field by default" do
@@ -17,6 +20,15 @@ RSpec.feature "Field Sorting", type: :feature do
 
       expect(page).to have_css 'th[data-control="resource-field-th"][data-table-header-field-id="id"][data-table-header-field-type="id"] div[data-sortable="false"]'
       expect(page).not_to have_css 'th[data-control="resource-field-th"][data-table-header-field-id="id"][data-table-header-field-type="id"] a svg'
+    end
+
+    it "sorts text while ignoring case" do
+      visit avo.resources_people_path(sort_by: "name", sort_direction: "asc")
+
+      expect(page).to have_css 'th[data-control="resource-field-th"][data-table-header-field-id="name"][data-table-header-field-type="text"]'
+      within "turbo-frame#people_list tbody" do
+        expect(page).to have_css "tr[data-resource-id=#{amy.id}] + tr[data-resource-id=#{bob.id}] + tr[data-resource-id=#{cathy.id}]"
+      end
     end
   end
 end
